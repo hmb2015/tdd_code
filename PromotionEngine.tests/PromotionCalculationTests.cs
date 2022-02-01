6c4f7540836a35b3ac5c45cbef7992cd6a0cdc23
Harsha Bellary <Harsha_Bellary@swissre.com>
2022-02-01 18:03:54 +0530

    Before implementing the promotionl price calculation logic

--- PromotionEngine.tests/PromotionCalculationTests.cs
+++ PromotionEngine.tests/PromotionCalculationTests.cs
@@ -22,16 +22,36 @@ namespace PromotionEngine.tests
         [TestMethod]
         public void CheckPriceOfOneItem()
         {
-            Cart items = new Cart(new List<Item> { new Item("A", 1, 50.0)});
+            Cart items = new Cart(new List<Item> { new Item("A", 1)});
             Assert.AreEqual(50.0, items.Total);
         }
 
         [TestMethod]
         public void CheckPriceOfMultipleItems()
         {
-            Cart items = new Cart(new List<Item> { new Item("A", 1, 50.0), new Item("B", 2, 30.0) });
+            Cart items = new Cart(new List<Item> { new Item("A", 1), new Item("B", 2) });
             Assert.AreEqual(110.0, items.Total);
         }
 
+        [TestMethod]
+        public void CheckPriceOfItemsNotAvailable()
+        {
+            Cart items = new Cart(new List<Item> { new Item("E", 1), new Item("G", 2) });
+            Assert.AreEqual(0.0, items.Total);
+        }
+
+        [TestMethod]
+        public void CheckPromotionPriceOfMultipleItems_Fail()
+        {
+            Cart items = new Cart(new List<Item> { new Item("A", 1), new Item("B", 2) });
+            Assert.AreNotEqual(110.0, items.TotalAfterPromotions);
+        }
+
+        [TestMethod]
+        public void CheckPromotionPriceOfMultipleItems1()
+        {
+            Cart items = new Cart(new List<Item> { new Item("A", 1), new Item("B", 2) });
+            Assert.AreEqual(95.0, items.TotalAfterPromotions);
+        }
     }
 }
--- PromotionEngine/PromotionClasses.cs
+++ PromotionEngine/PromotionClasses.cs
@@ -7,24 +7,61 @@ namespace PromotionEngine
 {
     public class PromotionClasses
     {
-        
+
     }
     public class Item
     {
-        public Item(string itemType, int itemQuantity, double itemPrice)
+        public Item(string itemType, int itemQuantity)
         {
             _itemQuantity = itemQuantity;
             _itemType = itemType;
-            _itemPrice = itemPrice;
         }
         private int _itemQuantity { get; set; }
         private string _itemType { get; set; }
-        private double _itemPrice { get; set; }
 
-
-        public static double GetSingleItemTotalPrice(Item item)
+        Dictionary<string, double> _itemPriceList = new Dictionary<string, double>()
         {
-            return (item._itemQuantity * item._itemPrice);
+            { "A", 50.0 }, { "B", 30.0 }, { "C", 20.0 }, { "D", 15.0 }
+        };
+
+        List<PromotionalPrice> activePromotions = new List<PromotionalPrice>
+        {
+            new PromotionalPrice("A", 3, 130.0),
+            new PromotionalPrice("B", 2, 45.0),
+            new PromotionalPrice("C", 1, 0.0),
+            new PromotionalPrice("D", 1, 15.0),
+        };
+
+        public double GetSingleItemTotalPrice(Item item)
+        {
+            return (item._itemQuantity * (_itemPriceList.ContainsKey(item._itemType) ? _itemPriceList[item._itemType] : 0.0));
+        }
+
+
+        public double GetPromotionalPrice(List<Item> items)
+        {
+            double totalPromotionPrice = 0.0;
+
+            foreach (var item in items)
+            {
+
+            }
+
+            return totalPromotionPrice;
+        }
+    }
+
+    public class PromotionalPrice
+    {
+        private int _itemQuantity { get; set; }
+        private string _itemType { get; set; }
+        private double _itemPromotionalPrice { get; set; }
+
+        public PromotionalPrice(string itemType, int itemQuantity, double itemPromotionalPrice)
+        {
+            _itemType = itemType;
+            _itemQuantity = itemQuantity;
+            _itemPromotionalPrice = itemPromotionalPrice;
         }
     }
 
@@ -38,7 +75,9 @@ namespace PromotionEngine
         }
 
         //public double Total => _items.Count > 0 ? (_items[0].ItemPrice * _items[0].ItemQuantity): 0.0;
-        public double Total => _items.Aggregate(0, (double total, Item item) => total + Item.GetSingleItemTotalPrice(item));
+        public double Total => _items.Aggregate(0, (double total, Item item) => total + item.GetSingleItemTotalPrice(item));
+
+        public double TotalAfterPromotions => _items.Aggregate(0, (double totalAfterPromotions, Item item) => totalAfterPromotions + item.GetPromotionalPrice(_items));
 
     }
 }
